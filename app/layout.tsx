e3c3bc111bf7b0fd2c951e44057a50d557df49d1
Mohammed-6726 <moodee6726@gmail.com>
2024-06-07 14:45:11 +0300

    init project files

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  subsets :['latin'],
+  weight :['400'],
+});
+
+export const metadata: Metadata = {
+  title: "MCoffee",
+  description: "We've 10 years' experience of crafting the finest quality coffee. From revolutionary methods and commitment to quality to unforgettable successes that have made Costa Coffee the Nation's Favourite coffee shop, our story is as unique as our coffee ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${openSans.className}`}>{children}</body>
+    </html>
+  );
+}
