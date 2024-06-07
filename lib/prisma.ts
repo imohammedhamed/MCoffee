faf49dfb3437ff576ee976088d1fc1be6482a296
Mohammed-6726 <moodee6726@gmail.com>
2024-06-07 23:24:39 +0300

    prisma connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient();
+import { withAccelerate } from '@prisma/extension-accelerate'
 
+const prisma = new PrismaClient().$extends(withAccelerate())
 export default prisma;
