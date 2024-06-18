eaca80bae2e395869f4a65cd38249dd1c0c9a52a
imohammedhamed <mohammedhamed6726@gmail.com>
2024-06-19 02:10:16 +0300

    add a new object for a new card

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,3 @@
-import SummerimgSrc from '../public/Summerdrinks-sections1.png'
 import { FaFacebook ,FaSquareInstagram ,FaLinkedin } from "react-icons/fa6";
 
 export const newsLetterCardContent = [
@@ -8,7 +7,7 @@ export const newsLetterCardContent = [
     subTitle:"Buy one fall drink, get one free",
     bodyContent:"Rewards members get one free handcrafted fall drink with purchase of any fall beverage, every Thursday afternoon in September.",
     buttonContent:"Oder Now",
-    SummerimgSrc,
+    imgSrc : "/Summerdrinks-sections1.png",
     isReverse:true,
     },
     {
@@ -17,8 +16,16 @@ export const newsLetterCardContent = [
     subTitle:"Buy 9 coffees and the 10th is on us!",
     bodyContent:"Start your day with a cup of coffee. Choose your favorite coffee here Get two coffee cups with a special discount",
     buttonContent:"Join Now",
-    SummerimgSrc,
+    imgSrc:"/illustration8.svg",
     isReverse:false,
+    },
+    {id:"3",
+    title :"Personal cups for good",
+    subTitle:"",
+    bodyContent:"Your choice is a positive and responsible one—because bringing your clean reusable cup helps our planet.",
+    buttonContent:"Learn More",
+    imgSrc:"/illustration7.svg",
+    isReverse:true,
     },
 ] as const
 
@@ -30,17 +37,17 @@ export const NavLinks = [
     },
     {
         Name:"Menu",
-        path:"/",
+        path:"/Menu",
         clicked:false,
     },
     {
         Name:"About Us",
-        path:"/",
+        path:"/#about",
         clicked:false,
     },
     {
         Name:"NewsLetter",
-        path:"/",
+        path:"/#newsletter",
         clicked:false,
     },
     {
@@ -67,3 +74,67 @@ export const SocialLinks =[
         hrefUrl:"/"
     },     
 ]as const 
+
+export const  ChoiceOfSize= [
+    {
+    id:"1",
+    name:"Small",
+    },
+    {
+    id:"2",
+    name:"Regular",
+    },
+    {
+    id:"3",
+    name:"Large",
+    },
+]as const;
+
+export const  ChoiceOfCoffee= [
+    {
+      id:"1",
+      name:"Signature Blend",
+    },
+    {
+      id:"2",
+      name:"Platinum Blend",
+    },
+    {
+      id:"3",
+      name:"Decaf",
+    },
+]as const;
+
+export const ExtraShot =[
+    {
+        id:"1",
+        name:"Add Signature Blend"
+    },
+    {
+        id:"2",
+        name:"Add Platinum Blend"
+    },
+    {
+        id:"3",
+        name:"Add Decaf"
+    },
+]as const 
+
+export const ChoiceOfMilk =[
+    {
+        id:"1",
+        name:"Full Cream Milk"
+    },
+    {
+        id:"2",
+        name:"Skimmed Milk"
+    },
+    {
+        id:"3",
+        name:"Oat Milk"
+    },
+    {
+        id:"4",
+        name:"No Milk"
+    },
+]
