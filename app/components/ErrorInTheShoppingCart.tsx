4ae36e57c610668505a5f50cccb12119992b975b
imohammedhamed <mohammedhamed6726@gmail.com>
2024-07-01 18:44:08 +0300

    edit the text in ErrorInTheShoppingCart component to the center

--- app/components/ErrorInTheShoppingCart.tsx
+++ app/components/ErrorInTheShoppingCart.tsx
@@ -10,7 +10,7 @@ interface ErrorInTheShoppingCartProps{
 }
 export default function ErrorInTheShoppingCart({title,LinkTitle,Linkhref}:ErrorInTheShoppingCartProps) {
   return (
-    <section className=' container mx-auto rounded-lg my-24 bg-white border border-solid border-lightBlue p-5 flex flex-col justify-center items-center'>
+    <section className=' container mx-auto rounded-lg my-24 bg-white border border-solid border-lightBlue p-5 flex flex-col justify-center items-center text-center'>
         <Image src={imgsrc} alt='user Cart is empty' className='py-10 rotate-90' width={250} height={250}/>
         <h1 className=' text-2xl text-Blue600'>{title}</h1>
         <p className=' text-base text-lightGrey font-bold pb-5'>What are you waiting for?</p>
